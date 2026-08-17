649d67054ecee39f1c7f2413be3ac49d6cb1d7a6
Mohomed-Zaid <zaidn2848@gmai.com>
2026-08-17 10:11:05 +0530

    sw.js commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-433ac1a8'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.eva1i3vrscg"
+    "revision": "0.f7esiphb83o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
